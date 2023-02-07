68dc3c9e6d7bb0ac94ebd483a75f66c6504d15e7
Juan Collantes <juan@collantes.ec>
2023-02-07 00:14:18 -0500

    index updated

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import {defineConfig} from 'vite'
+export default defineConfig({
+    //Es el directorio de la pagina web dentro del repositorio
+    //https://jotacollantes.github.io/blackjack-vite/
+    base: '/blackjack-vite/',
+    plugins:[]
+})
